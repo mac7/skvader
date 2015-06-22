36943fb0d10c44cf7343f7058eb41b30bf8513c3
Maxim Sosedov <sosedow@gmail.com>
2015-06-22 23:41:00 +0300

    output+pong algorithm was modified

--- skvader.py
+++ skvader.py
@@ -7,14 +7,18 @@ import utils
 
 
 def main(argv):
-	div = '_'*76
-	print '%s\n%s\n%s' % (div, utils.pingpong(argv[0]).rstrip(), div)
-
+	div = '_' * 32 + '\n'
+	print '%s\nPacket loss = %s\nrtt min = %s ms\nrtt avg = %s ms\nrtt max = %s ms\njitter (avg) = %s ms\n%s' % (div,
+																									 utils.pingpong(argv[0])[0],
+																									 utils.pingpong(argv[0])[1],
+																									 utils.pingpong(argv[0])[2],
+																									 utils.pingpong(argv[0])[3],
+																									 utils.pingpong(argv[0])[4],
+																									 div)
 
 
 if __name__ == '__main__':
 	try:
-		print 'Please wait...'
 		main(sys.argv[1:])
 	except:
 		print """
--- utils.py
+++ utils.py
@@ -1,26 +1,33 @@
 import subprocess
 import platform
 
-def pingpong(host):
 
+def pingpong(host):
 	if host == 'leo':
 		host = '192.168.248.103'
 	elif host == 'r2d2':
 		host = '192.168.248.152'
 	if platform.system() == 'Linux':
 		ping = subprocess.Popen(
-	    				["ping", "-A","-c", "10", host],
-	    				stdout = subprocess.PIPE,
-	    				stderr = subprocess.PIPE
-					)
+			["ping", "-A", "-c", "10", host],
+			stdout=subprocess.PIPE,
+			stderr=subprocess.PIPE
+		)
 		out, error = ping.communicate()
-		return out[out.find('---'):]
+		flag = out.find('min/avg/max/mdev')
+		loss = out[out.find('packet loss') - 3 : out.find('packet loss') - 1]
+		rttMin = out[flag + 19 : flag + 25]
+		rttMax = out[flag + 33 : flag + 39]
+		rttAvg = out[flag + 26 : flag + 32]
+		jitter = str(((float(rttAvg) - float(rttMin)) + (float(rttMax) - float(rttAvg))) / 2)
+		return [loss, rttMin, rttAvg, rttMax, jitter]
+
 	else:
 		ping = subprocess.Popen(
-	    				["ping", "-n", "10", host],
-	    				stdout = subprocess.PIPE,
-	    				stderr = subprocess.PIPE
-					)
+			["ping", "-n", "10", host],
+			stdout=subprocess.PIPE,
+			stderr=subprocess.PIPE
+		)
 		out, error = ping.communicate()
-        return out[out.find('Ping statistics'):]
+	return out[out.find('Ping statistics'):]
 
