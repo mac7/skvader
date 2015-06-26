aa629eea2772f5027b10ea6879bdcf15e356108f
mac7 <sosedow@gmail.com>
2015-06-26 09:32:33 +0300

    @

--- skvader.py
+++ skvader.py
@@ -7,21 +7,17 @@ import utils
 
 
 def main(argv):
-	div = '_' * 32 + '\n'
-	print '%s\nPacket loss = %s\nrtt min = %s ms\nrtt avg = %s ms\nrtt max = %s ms\njitter (avg) = %s ms\n%s' % (div,
-																									 utils.pingpong(argv[0])[0],
-																									 utils.pingpong(argv[0])[1],
-																									 utils.pingpong(argv[0])[2],
-																									 utils.pingpong(argv[0])[3],
-																									 utils.pingpong(argv[0])[4],
-																									 div)
+    div = '_' * 32 + '\n'
+    output =  utils.pingpong(argv[0])
+    for i in [0,6,12]:
+        print '%s\nPacket size = %s\nPacket loss = %s\nrtt min = %s ms\nrtt max = %s ms\nrtt avg = %s ms\njitter (avg) = %s ms\n%s\n' % (div, output[i], output[i+1], output[i+2], output[i+3], output[i+4], output[i+5], div)
 
 
 if __name__ == '__main__':
-	try:
+#	try:
 		main(sys.argv[1:])
-	except:
-		print """
-		Usage:   
-                skvader.py -host
-"""
+#	except:
+#		print """
+#		Usage:
+#                skvader.py -host
+#"""
--- utils.py
+++ utils.py
@@ -3,31 +3,56 @@ import platform
 
 
 def pingpong(host):
-	if host == 'leo':
-		host = '192.168.248.103'
-	elif host == 'r2d2':
-		host = '192.168.248.152'
-	if platform.system() == 'Linux':
-		ping = subprocess.Popen(
-			["ping", "-A", "-c", "10", host],
-			stdout=subprocess.PIPE,
-			stderr=subprocess.PIPE
-		)
-		out, error = ping.communicate()
-		flag = out.find('min/avg/max/mdev')
-		loss = out[out.find('packet loss') - 3 : out.find('packet loss') - 1]
-		rttMin = out[flag + 19 : flag + 25]
-		rttMax = out[flag + 33 : flag + 39]
-		rttAvg = out[flag + 26 : flag + 32]
-		jitter = str(((float(rttAvg) - float(rttMin)) + (float(rttMax) - float(rttAvg))) / 2)
-		return [loss, rttMin, rttAvg, rttMax, jitter]
+    """
 
-	else:
-		ping = subprocess.Popen(
-			["ping", "-n", "10", host],
-			stdout=subprocess.PIPE,
-			stderr=subprocess.PIPE
-		)
-		out, error = ping.communicate()
-	return out[out.find('Ping statistics'):]
+    """
+    if host == 'leo':
+        host = '192.168.248.103'
+    elif host == 'r2d2':
+        host = '192.168.248.152'
 
+    statistic = []
+
+    if platform.system() == 'Linux':
+        ping = subprocess.Popen(
+            ["ping", "-A", "-c", "10", host],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        out, error = ping.communicate()
+        flag = out.find('min/avg/max/mdev')
+        loss = out[out.find('packet loss') - 3: out.find('packet loss') - 1]
+        rttMin = out[flag + 19: flag + 25]
+        rttMax = out[flag + 33: flag + 39]
+        rttAvg = out[flag + 26: flag + 32]
+        jitter = str(((float(rttAvg) - float(rttMin)) + (float(rttMax) - float(rttAvg))) / 2)
+        return [64, loss, rttMin, rttAvg, rttMax, jitter]
+
+    else:
+        for foo in [64, 782, 1500]:
+            ping = subprocess.Popen(
+                ["ping", "-l", str(foo), "-n", "10", host],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+            out, error = ping.communicate()
+            statistic.append(foo)
+            statistic.append(out[out.find('(', out.find('Lost')) + 1: out.find('loss') - 1])
+            if 'Approximate round trip times in milli-seconds' in out:
+                rttMin = out[out.find('Minimum') + 10: out.find('ms', out.find('Minimum'))]
+                rttMax = out[out.find('Maximum') + 10: out.find('ms', out.find('Maximum'))]
+                rttAvg = out[out.find('Average') + 10: out.find('ms', out.find('Average'))]
+                jitter = str(((float(rttAvg) - float(rttMin)) + (float(rttMax) - float(rttAvg))) / 2)
+                statistic.append(rttMin)
+                statistic.append(rttMax)
+                statistic.append(rttAvg)
+                statistic.append(jitter)
+
+                print statistic
+            else:
+                for i in range(0, 5):
+                    statistic.append('host unreachable')
+                print statistic
+
+    print 'pong is done'
+    return statistic
